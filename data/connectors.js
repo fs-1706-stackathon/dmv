53666fc9cdc25d38d161e87dbe520695a27e3931
Joon Kim <joon3575@gmail.com>
2017-09-24 12:20:59 -0500

    Completed updatinng the theme

--- data/connectors.js
+++ data/connectors.js
@@ -1,11 +1,11 @@
 import Sequelize from 'sequelize';
-// import casual from 'casual';
+import casual from 'casual';
 import _ from 'lodash';
 import fetch from 'node-fetch';
 
 const db = new Sequelize('dmv', null, null, {
   dialect: 'sqlite',
-  storage: './blog.sqlite',
+  storage: './dmv.sqlite',
 });
 
 const DriverModel = db.define('driver', {
@@ -14,43 +14,29 @@ const DriverModel = db.define('driver', {
 });
 
 const CarModel = db.define('car', {
-  title: { type: Sequelize.STRING },
-  text: { type: Sequelize.STRING },
-  regNumber: { type: Sequelize.INTEGER },
+  make: { type: Sequelize.STRING },
+  model: { type: Sequelize.STRING },
+  vin: { type: Sequelize.STRING },
 });
 
 DriverModel.hasMany(CarModel);
 CarModel.belongsTo(DriverModel);
 
-// // create mock data with a seed, so we always get the same
-// casual.seed(123);
-// db.sync({ force: true }).then(() => {
-//   _.times(10, () => {
-//     return AuthorModel.create({
-//       firstName: casual.first_name,
-//       lastName: casual.last_name,
-//     }).then((author) => {
-//       return author.createPost({
-//         title: `A post by ${author.firstName}`,
-//         text: casual.sentences(3),
-//       });
-//     });
-//   });
-// });
-
-
-// // add this somewhere in the middle
-// const FortuneCookie = {
-//   getOne() {
-//     return fetch('http://fortunecookieapi.herokuapp.com/v1/cookie')
-//       .then(res => res.json())
-//       .then(res => {
-//         console.log(res[0].fortune.message);
-//         return res[0].fortune.message;
-//       });
-//   },
-// };
-
+// create mock data with a seed, so we always get the same
+casual.seed(123);
+db.sync({ force: true }).then(() => {
+  _.times(10, () => {
+    return DriverModel.create({
+      firstName: casual.first_name,
+      lastName: casual.last_name,
+    }).then((driver) => {
+      return driver.createCar({
+        make: `A car by ${driver.firstName}`,
+        model: casual.sentences(1),
+      });
+    });
+  });
+});
 
 const Driver = db.models.driver;
 const Car = db.models.car;
