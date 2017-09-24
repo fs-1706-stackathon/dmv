7eb1142259bfdb8bcee119ad4e19f62c29e94bd8
Joon Kim <joon3575@gmail.com>
2017-09-24 11:14:05 -0500

    Changed the theme to dmv

--- data/connectors.js
+++ data/connectors.js
@@ -1,58 +1,59 @@
 import Sequelize from 'sequelize';
-import casual from 'casual';
+// import casual from 'casual';
 import _ from 'lodash';
 import fetch from 'node-fetch';
 
-const db = new Sequelize('blog', null, null, {
+const db = new Sequelize('dmv', null, null, {
   dialect: 'sqlite',
   storage: './blog.sqlite',
 });
 
-const AuthorModel = db.define('author', {
+const DriverModel = db.define('driver', {
   firstName: { type: Sequelize.STRING },
   lastName: { type: Sequelize.STRING },
 });
 
-const PostModel = db.define('post', {
+const CarModel = db.define('car', {
   title: { type: Sequelize.STRING },
   text: { type: Sequelize.STRING },
+  regNumber: { type: Sequelize.INTEGER },
 });
 
-AuthorModel.hasMany(PostModel);
-PostModel.belongsTo(AuthorModel);
+DriverModel.hasMany(CarModel);
+CarModel.belongsTo(DriverModel);
 
-// create mock data with a seed, so we always get the same
-casual.seed(123);
-db.sync({ force: true }).then(() => {
-  _.times(10, () => {
-    return AuthorModel.create({
-      firstName: casual.first_name,
-      lastName: casual.last_name,
-    }).then((author) => {
-      return author.createPost({
-        title: `A post by ${author.firstName}`,
-        text: casual.sentences(3),
-      });
-    });
-  });
-});
-
-
-// add this somewhere in the middle
-const FortuneCookie = {
-  getOne() {
-    return fetch('http://fortunecookieapi.herokuapp.com/v1/cookie')
-      .then(res => res.json())
-      .then(res => {
-        console.log(res[0].fortune.message);
-        return res[0].fortune.message;
-      });
-  },
-};
+// // create mock data with a seed, so we always get the same
+// casual.seed(123);
+// db.sync({ force: true }).then(() => {
+//   _.times(10, () => {
+//     return AuthorModel.create({
+//       firstName: casual.first_name,
+//       lastName: casual.last_name,
+//     }).then((author) => {
+//       return author.createPost({
+//         title: `A post by ${author.firstName}`,
+//         text: casual.sentences(3),
+//       });
+//     });
+//   });
+// });
 
 
-const Author = db.models.author;
-const Post = db.models.post;
+// // add this somewhere in the middle
+// const FortuneCookie = {
+//   getOne() {
+//     return fetch('http://fortunecookieapi.herokuapp.com/v1/cookie')
+//       .then(res => res.json())
+//       .then(res => {
+//         console.log(res[0].fortune.message);
+//         return res[0].fortune.message;
+//       });
+//   },
+// };
+
+
+const Driver = db.models.driver;
+const Car = db.models.car;
 
 // and at the bottom, modify the export to include FortuneCookie
-export { Author, Post, FortuneCookie };
+export { Driver, Car };
